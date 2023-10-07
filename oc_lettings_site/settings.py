7e65a36b2529d8f20ea604b76507f86fb5758ce0
Denizot <denizot.myriam@gmail.com>
2023-10-07 15:58:23 +0200

    update de

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -139,8 +139,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-#
-#
+
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
