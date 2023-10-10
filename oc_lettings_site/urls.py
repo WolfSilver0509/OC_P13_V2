402b39505776c0b6803767e72876803d30e699a1
Denizot <denizot.myriam@gmail.com>
2023-10-10 16:42:51 +0200

    update sentry-debug

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.shortcuts import render
 
 def trigger_error(request):
     _ = 1 / 0
+    return render(request, 'templates/error500.html')
+
 
 
 urlpatterns = [
