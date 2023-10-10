778a5a629082a8a2f1a9d22677e4a306de4ca715
Denizot <denizot.myriam@gmail.com>
2023-10-11 01:36:14 +0200

    litting ok rajout flake8 linting

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -5,14 +5,13 @@ from django.shortcuts import render
 
 import sentry_sdk
 
+
 def trigger_error(request):
     try:
         _ = 1 / 0
     except ZeroDivisionError as error:
         sentry_sdk.capture_exception(error)  # Capturez l'erreur complète
         return render(request, 'error500.html')
-
-
 
 
 urlpatterns = [
