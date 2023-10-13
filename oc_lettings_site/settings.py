d521e097ac7d65a9e8a002588d569ce3754fabcc
Denizot <denizot.myriam@gmail.com>
2023-10-13 14:20:20 +0200

    preparation soutenance test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -10,7 +10,7 @@ sentry_sdk.init(
     dsn=os.getenv('DNS'),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
-    traces_sample_rate=0.1,
+    traces_sample_rate=1.0,
     # Set profiles_sample_rate to 1.0 to profile 100%pip install load-dotenv
     # of sampled transactions.
     # We recommend adjusting this value in production.
